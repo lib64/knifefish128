f8723c8aab922a492ff09e4b3336f16a522a5208
lib64 <michael.harper2@g.austincc.edu>
2019-06-29 22:34:17 -0500

    set unchanging local vars to const

--- src/kf128.c
+++ src/kf128.c
@@ -8,6 +8,7 @@
 #include <string.h>
 
 void kf_lfsr(uint32_t *shift_register) {
+
   *shift_register = (((((*shift_register >> 31) ^ (*shift_register >> 6) ^
                         (*shift_register >> 4) ^ (*shift_register >> 2) ^
                         (*shift_register >> 1) ^ *shift_register)) &
@@ -17,25 +18,31 @@ void kf_lfsr(uint32_t *shift_register) {
 }
 
 uint8_t kf_lfsr_byte(uint32_t *shift_register) {
+
   for (int i = 0; i < 8; i++)
     kf_lfsr(shift_register);
+
   return *shift_register & 0x000000FF;
 }
 
 void kf_pht(const uint32_t *a, const uint32_t *b, uint32_t *a_prime,
             uint32_t *b_prime) {
-  uint32_t ap = (*a) + (*b) % PHT_MAX;
-  uint32_t bp = *b_prime = (*a) + 2 * (*b) % PHT_MAX;
+
+  const uint32_t ap = (*a) + (*b) % PHT_MAX;
+  const uint32_t bp = *b_prime = (*a) + 2 * (*b) % PHT_MAX;
   *a_prime = ap;
   *b_prime = bp;
 }
 
 void kf_invert_ctx(const kf_ctx *key, kf_ctx *inv) {
+
   memcpy(inv, key, sizeof(kf_ctx));
+
   for (int i = 0; i < ROUNDS; i++) {
     inv->skey[i][0] = key->skey[ROUNDS - i - 1][0];
     inv->skey[i][1] = key->skey[ROUNDS - i - 1][1];
   }
+
   for (int i = 0; i < 4; i++) {
     inv->wkey[0][i] = key->wkey[1][i];
     inv->wkey[1][i] = key->wkey[0][i];
@@ -43,6 +50,7 @@ void kf_invert_ctx(const kf_ctx *key, kf_ctx *inv) {
 }
 
 void kf_init_sbox(uint8_t s[SBOX_SIZE], const uint32_t seed) {
+
   uint8_t indexes[SBOX_SIZE];
 
   uint32_t seed_copy = seed;
@@ -52,13 +60,14 @@ void kf_init_sbox(uint8_t s[SBOX_SIZE], const uint32_t seed) {
   }
 
   for (int i = 0; i < SBOX_SIZE; i++) {
-    size_t index = kf_lfsr_byte(&seed_copy) % (SBOX_SIZE - i);
+    const size_t index = kf_lfsr_byte(&seed_copy) % (SBOX_SIZE - i);
     s[i] = indexes[index];
     memmove(indexes + index, indexes + index + 1, SBOX_SIZE - index - 1);
   }
 }
 
 void kf_init_pbox(uint8_t p[PBOX_SIZE], const uint32_t seed) {
+
   uint8_t indexes[PBOX_SIZE];
 
   uint32_t seed_copy = seed;
@@ -68,21 +77,19 @@ void kf_init_pbox(uint8_t p[PBOX_SIZE], const uint32_t seed) {
   }
 
   for (int i = 0; i < PBOX_SIZE; i++) {
-    size_t index = kf_lfsr_byte(&seed_copy) % (PBOX_SIZE - i);
+    const size_t index = kf_lfsr_byte(&seed_copy) % (PBOX_SIZE - i);
     p[i] = indexes[index];
     memmove(indexes + index, indexes + index + 1, PBOX_SIZE - index - 1);
   }
 }
 
 void kf_expand_passphrase(const char *passphrase, kf_ctx *ctx) {
+
   uint32_t key[KEY_SIZE] = {0};
 
   uint32_t shift_register = 0;
 
-  size_t passlen = strlen(passphrase);
-
-  if (passlen > 256)
-    passlen = 256;
+  const size_t passlen = (strlen(passphrase) > 256) ? 256 : strlen(passphrase);
 
   for (size_t i = 0; i < passlen - 4; i += 4) {
     shift_register =
@@ -114,6 +121,7 @@ void kf_expand_passphrase(const char *passphrase, kf_ctx *ctx) {
   }
 
   int count = 0;
+
   for (int i = 0; i < SBOX_COUNT; i++) {
     kf_init_sbox(ctx->sbox[i], key[count++]);
   }
@@ -134,7 +142,9 @@ void kf_expand_passphrase(const char *passphrase, kf_ctx *ctx) {
 }
 
 void kf_f(const uint32_t *in, uint32_t *out, const size_t round, kf_ctx *ctx) {
+
   const uint8_t *in8 = (const uint8_t *)in;
+
   uint8_t *out8 = (uint8_t *)out;
 
   for (int i = 0; i < SBOX_COUNT; i++) {
@@ -149,6 +159,7 @@ void kf_f(const uint32_t *in, uint32_t *out, const size_t round, kf_ctx *ctx) {
 
 void kf_round(const uint32_t *in, uint32_t *out, const size_t round,
               kf_ctx *ctx) {
+
   uint32_t tmp[2];
 
   kf_f(in + 2, tmp, round, ctx);
@@ -166,6 +177,7 @@ void kf_round(const uint32_t *in, uint32_t *out, const size_t round,
 }
 
 void kf_block(const uint32_t *in, uint32_t *out, kf_ctx *ctx) {
+
   memcpy(out, in, sizeof(uint32_t) * 4);
 
   out[0] ^= ctx->wkey[0][0];
@@ -186,6 +198,7 @@ void kf_block(const uint32_t *in, uint32_t *out, kf_ctx *ctx) {
 void kf_encrypt_file_cbc(const char *infile, const char *outfile,
                          const char *passphrase, const char *iv,
                          const char *padding) {
+
   kf_ctx ctx;
   kf_expand_passphrase(passphrase, &ctx);
 
@@ -195,17 +208,18 @@ void kf_encrypt_file_cbc(const char *infile, const char *outfile,
   fwrite(iv, sizeof(char), BLOCK_SIZE, out);
 
   fseek(in, 0L, SEEK_END);
-  long input_size = ftell(in);
+  const long input_size = ftell(in);
   rewind(in);
 
-  long remaining = input_size % BLOCK_SIZE;
-  long block_count = input_size / BLOCK_SIZE + (remaining > 0);
+  const long remaining = input_size % BLOCK_SIZE;
+  const long block_count = input_size / BLOCK_SIZE + (remaining > 0);
 
   uint32_t block[4], key[4], tmp[4];
 
   memcpy(key, iv, sizeof(uint32_t) * 4);
 
   for (int i = 0; i < block_count; i++) {
+
     if (i == block_count - 1 && remaining != 0) {
       memcpy(block, padding, sizeof(uint32_t) * 4);
       fread(block, sizeof(uint8_t), remaining, in);
@@ -245,6 +259,7 @@ void kf_encrypt_file_cbc(const char *infile, const char *outfile,
 
 void kf_decrypt_file_cbc(const char *infile, const char *outfile,
                          const char *passphrase) {
+
   kf_ctx ctx, inv;
   kf_expand_passphrase(passphrase, &ctx);
   kf_invert_ctx(&ctx, &inv);
@@ -253,16 +268,17 @@ void kf_decrypt_file_cbc(const char *infile, const char *outfile,
   FILE *out = fopen(outfile, "wb");
 
   fseek(in, 0L, SEEK_END);
-  long input_size = ftell(in);
+  const long input_size = ftell(in);
   rewind(in);
 
   uint32_t block[4], key[4], tmp[4];
 
   fread(key, sizeof(uint32_t), 4, in);
 
-  long block_count = input_size / BLOCK_SIZE - 1;
+  const long block_count = input_size / BLOCK_SIZE - 1;
 
   for (int i = 0; i < block_count; i++) {
+
     fread(block, sizeof(uint32_t), 4, in);
 
     kf_block(block, tmp, &inv);
@@ -275,7 +291,7 @@ void kf_decrypt_file_cbc(const char *infile, const char *outfile,
     if (i != block_count - 1) {
       fwrite(tmp, sizeof(uint32_t), 4, out);
     } else {
-      uint8_t remaining = ((uint8_t *)tmp)[BLOCK_SIZE - 1];
+      const uint8_t remaining = ((uint8_t *)tmp)[BLOCK_SIZE - 1];
       fwrite(tmp, sizeof(uint8_t), remaining, out);
     }
     memcpy(key, block, sizeof(uint32_t) * 4);
